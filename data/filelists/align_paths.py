170902c475ed2af6470603b27f6737f61e21c272
shenranw <wsr.20021128@gmail.com>
2024-06-21 16:42:24 -0700

    align path abstraction

--- data/filelists/align_paths.py
+++ data/filelists/align_paths.py
@@ -2,16 +2,18 @@ def align_path(path_to_data, path_to_filelist):
     lines = []
     with open(path_to_filelist, "r") as file:
         for line in file:
+            dirs = line.split("/")
+            line = dirs[len(dirs) - 1]
             line = path_to_data + line
             lines.append(line)
     with open(path_to_filelist, "w") as file:
         for line in lines:
             file.write(line)
             
-multilingual_data_path = "/project/6080355/shenranw/data/"
-multilingual_train_filelist_path = "./mikmaw_train_filelist.txt"
-multilingual_test_filelist_path = "./mikmaw_test_filelist.txt"
-multilingual_val_filelist_path = "./mikmaw_val_filelist.txt"
+multilingual_data_path = "../data/"
+multilingual_train_filelist_path = "./multilingual_train_filelist.txt"
+multilingual_test_filelist_path = "./multilingual_test_filelist.txt"
+multilingual_val_filelist_path = "./multilingual_val_filelist.txt"
 align_path(multilingual_data_path, multilingual_train_filelist_path)
 align_path(multilingual_data_path, multilingual_test_filelist_path)
 align_path(multilingual_data_path, multilingual_val_filelist_path)
