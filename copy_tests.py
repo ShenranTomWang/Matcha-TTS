050c11759c5e32d599c0a3b0f0f560359ba96bb4
shenranw <wsr.20021128@gmail.com>
2024-06-23 20:09:22 -0700

    copy test bugfix

--- copy_tests.py
+++ copy_tests.py
@@ -2,18 +2,18 @@ import os
 import shutil
 
 # Read the list of files from the .txt file
-with open("data/filelists/objiwe_audio_text_test_filelist.txt", "r") as file:
+with open("./data/filelists/multilingual_test_filelist.txt", "r", encoding="utf-8") as file:
     files_list = file.readlines()
 
 # Define the destination directory
-destination_dir = "/project/6080355/shenranw/test_samples"
+destination_dir = "../synth_output/synth_output-multilingual-matcha-hifigan-balanced-dataset/"
 
 # Iterate over each line in the list of files
 for line in files_list:
-    directory, description = line.strip().split("|")  # Split directory path and description
+    directory = line.split("|")[0]  # Split directory path and description
     dirs = directory.split("/")
     name = dirs[len(dirs) - 1].split(".")[0]
-    destination_file = os.path.join(destination_dir, f"{name}.wav")  # Create destination file path
-    shutil.copyfile(directory, destination_file)  # Copy file to destination directory
+    destination_file = os.path.join(destination_dir, f"{name}_real.wav")  # Create destination file path
+    shutil.copy(directory, destination_file)  # Copy file to destination directory
 
 print("Files copied successfully!")
