7ee2d773a9b1b6601fe2931b0e8f5f83b673e4d0
ShenranTomWang <wsr.20021128@gmail.com>
2024-06-23 22:10:01 -0400

    sync updated

--- sync_wandb.py
+++ sync_wandb.py
@@ -1,9 +1,9 @@
 import wandb
 
-WANDB_PROJECT = "MatchaTTS-Vocos"
-WANDB_NAME = "Multilingual Experiment A100"
+WANDB_PROJECT = "MatchaTTS-HiFiGAN"
+WANDB_NAME = "Multilingual Experiment A100 Balanced Dataset"
 WANDB_DATASET = "multilingual-test"
-WANDB_ARCH = "MatchaTTS: language embedding, Vocos: vanilla"
+WANDB_ARCH = "MatchaTTS: language embedding, HiFiGAN: vanilla, general"
 device = "a100"
 
 if __name__ == "__main__":
@@ -12,38 +12,40 @@ if __name__ == "__main__":
         name=WANDB_NAME,
         config={"architecture": WANDB_ARCH, "dataset": WANDB_DATASET, "hardware": device},
     )
-    scores = {
-        "AT/stoi": 0.034467383276918964,
-        "AT/pesq": 1.363548581600189,
-        "AT/mcd": 16.88833688779086,
-        "AT/f0_rmse": 74.89061267037675,
-        "AT/las_rmse": 7.055516948463254,
-        "AT/vuv_f1": 0.7355746955585164,
-        "MJ/stoi": 0.03716078347668129,
-        "MJ/pesq": 1.1734014856815338,
-        "MJ/mcd": 21.607332695152845,
-        "MJ/f0_rmse": 136.14104101854554,
-        "MJ/las_rmse": 7.237321248480112,
-        "MJ/vuv_f1": 0.6754111697184492,
-        "JJ/stoi": 0.03586410085988776,
-        "JJ/pesq": 1.3102074790000915,
-        "JJ/mcd": 18.78067436443153,
-        "JJ/f0_rmse": 57.59042086985218,
-        "JJ/las_rmse": 5.256278871686704,
-        "JJ/vuv_f1": 0.8345648743704168,
-        "NJ/stoi": 0.03309164618486104,
-        "NJ/pesq": 1.1419564771652222,
-        "NJ/mcd": 17.36092230950615,
-        "NJ/f0_rmse": 87.35175754309842,
-        "NJ/las_rmse": 6.174249493614179,
-        "NJ/vuv_f1": 0.8293946476192314,
-        "rtfs_mean": 0.038103,
-        "rtfs_std": 0.234020,
-        "rtfs_w_mean": 0.061516,
-        "rtfs_w_std": 0.433657,
+    speaker_scores = {
+        "AT/stoi": 0.03476608173971536,
+        "AT/pesq": 1.3493842947483063,
+        "AT/mcd": 17.68556912162006,
+        "AT/f0_rmse": 75.88562759206604,
+        "AT/las_rmse": 7.984762466948555,
+        "AT/vuv_f1": 0.7256836658617088,
+        "MJ/stoi": 0.038799910052540354,
+        "MJ/pesq": 1.137168389558792,
+        "MJ/mcd": 21.586509478161382,
+        "MJ/f0_rmse": 136.81219217901196,
+        "MJ/las_rmse": 7.5881106934620925,
+        "MJ/vuv_f1": 0.6254860752899867,
+        "JJ/stoi": 0.03870952746060171,
+        "JJ/pesq": 1.2363521265983581,
+        "JJ/mcd": 18.632428179269404,
+        "JJ/f0_rmse": 58.00454544422187,
+        "JJ/las_rmse": 4.929845884924541,
+        "JJ/vuv_f1": 0.8333261497732738,
+        "NJ/stoi": 0.04085142416153977,
+        "NJ/pesq": 1.124665106534958,
+        "NJ/mcd": 17.486743082985537,
+        "NJ/f0_rmse": 89.88091947113182,
+        "NJ/las_rmse": 6.35434139649528,
+        "NJ/vuv_f1": 0.8145662591581504
+    }
+    general_scores = {
+        "rtfs_mean": 0.030972,
+        "rtfs_std": 0.095306,
+        "rtfs_w_mean": 0.054327,
+        "rtfs_w_std": 0.244068,
         "num_ode_steps": 10,
         "temperature": 0.667,
-        "length_scale": 1.0,
+        "length_scale": 1.0
     }
-    wandb.log(scores)
-    
+    wandb.log(speaker_scores)
+    wandb.log(general_scores)
